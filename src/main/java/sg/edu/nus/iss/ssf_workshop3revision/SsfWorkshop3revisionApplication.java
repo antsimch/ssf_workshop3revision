e6fa2128c2a4d984daa63b08156fb9efd9bd1bc7
antsimch <anthonysimch@gmail.com>
2023-05-24 16:02:54 +0800

    task1

--- src/main/java/sg/edu/nus/iss/ssf_workshop3revision/SsfWorkshop3revisionApplication.java
+++ src/main/java/sg/edu/nus/iss/ssf_workshop3revision/SsfWorkshop3revisionApplication.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss.ssf_workshop3revision;
 
+import java.io.File;
+import java.util.List;
+
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SsfWorkshop3revisionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SsfWorkshop3revisionApplication.class, args);
-	}
 
+		SpringApplication app = new SpringApplication(SsfWorkshop3revisionApplication.class);
+
+		DefaultApplicationArguments appArgs = new DefaultApplicationArguments(args);
+
+		List<String> opsValues = appArgs.getOptionValues("dataDir");
+
+		String dataDir;
+
+		if ((opsValues == null) || (opsValues.get(0) == null)) {
+			System.out.println("args[0]: dataDir");
+			System.exit(1);
+		}
+
+		dataDir = opsValues.get(0);
+		
+		File newDir = new File(dataDir);
+
+		if (!newDir.exists()) {
+			newDir.mkdirs();
+		}
+
+		app.run(args);
+	}
 }
